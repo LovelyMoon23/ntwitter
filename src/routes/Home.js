c83367e38d66c86a4b33b73a4a8ffb40acf47398
LovelyMoon23 <moonsirl9123@gmail.com>
2022-03-08 16:56:37 +0900

    nweets 화면에 표시하기

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,8 +1,24 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { dbService } from '../fBase'
 
 const Home = () => {
   const [nweet, setNweet] = useState('')
+  const [nweets, setNweets] = useState([])
+
+  useEffect(() => {
+    getNweets()
+  }, [])
+  const getNweets = async () => {
+    const dbNweets = await dbService.collection('nweets').get()
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      }
+      setNweets((prev) => [nweetObject, ...prev])
+    })
+  }
+
   const onSubmit = async (event) => {
     event.preventDefault()
     await dbService.collection('nweets').add({
@@ -24,9 +40,14 @@ const Home = () => {
         type="text"
         placeholder="What's on your mind?"
         onChange={onChange}
-        maxLengh={120}
+        maxLength={120}
       ></input>
+
+      {nweets.nweet}
       <input type="submit" value="Nweet"></input>
+      {nweets.map((nweet) => (
+        <div key={nweet.id}>{nweet.nweet}</div>
+      ))}
     </form>
   )
 }
