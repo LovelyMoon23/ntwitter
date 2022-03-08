53813dc7376c6e0be3e6b50739ef80b99ee5f7e1
LovelyMoon23 <moonsirl9123@gmail.com>
2022-03-08 15:12:58 +0900

    form and database setup

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { dbService } from '../fBase'
 
 const Home = () => {
-  return <div>Home</div>
+  const [nweet, setNweet] = useState('')
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    await dbService.collection('nweets').add({
+      nweet,
+      createdAt: Date.now(),
+    })
+    setNweet('')
+  }
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event
+    setNweet(value)
+  }
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        value={nweet}
+        type="text"
+        placeholder="What's on your mind?"
+        onChange={onChange}
+        maxLengh={120}
+      ></input>
+      <input type="submit" value="Nweet"></input>
+    </form>
+  )
 }
 
 export default Home
